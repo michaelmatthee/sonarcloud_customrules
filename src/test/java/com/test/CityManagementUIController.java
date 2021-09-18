b9490ae6881c29117ae18c5aa3dc027dcbe4e264
michaelmatthee <michael.h.matthee@protonmail.com>
2021-09-18 16:08:30 -0400

    Create CityManagementUIController.java

---
+++ src/test/java/com/test/CityManagementUIController.java
@@ -0,0 +1,61 @@
+package com.test;                           
+
+/**                                                                 
+ * Hello world!
+ *
+ */
+public class CityManagementUIController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void test()
+    {
+       System.out.println( "Hello Mike!" );
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
