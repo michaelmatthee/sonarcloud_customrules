e2bf6ae64199fc012941fa6ce846885e919693e1
michaelmatthee <michael.h.matthee@protonmail.com>
2021-09-18 15:37:25 -0400

    Update CityManagementUIController.java

--- src/main/java/com/test/CityManagementUIController.java
+++ src/main/java/com/test/CityManagementUIController.java
@@ -1,5 +1,22 @@
+package com.test;                           
 
+/**                                                                 
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 
+    public static void test()
+    {
+       System.out.println( "Hello Mike!" );
+    }
+    
+}
 
 
 
